8269cd0ffb8d8e4d77bd5810539482b6fe4e0f5d
KozlovaNastya <nastya.kozlic.196@gmail.com>
2024-11-13 22:53:31 +0300

    Add files via upload

---
+++ Friendly_Numbers/FriendlyNumbers.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, ".1251");
+	int N;
+	int s1, s2, a, b;
+	int count = 0;
+	cout << "Введите предел" << endl;
+	cin >> N;
+	for (int i = 1; i < N; i++) {
+		s1 = 0;
+		for (a = 1; a < i-1; a++) {
+			if (i % a == 0)
+				s1 = s1 + a;
+		}
+		s2 = 0;
+		for (b = 1; b < s1-1; b++) {
+			if (s1 % b == 0)
+				s2 = s2 + b;
+		}
+		if (s1 > i && s2 == i && s1 != i) {
+			cout << "Числа " << s2 << " и " << s1 << " дружественные" << endl;
+			count++;
+		}
+	}
+	if(count == 0)
+		cout << "Нет дружественных чисел в заданном диапазоне" << endl;
+	return 0;
+}
